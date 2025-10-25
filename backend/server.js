067a4fc84a09ec628db61d05b02033fae595dbf3
Ahamed IbrahimKhan <ahamedibrahim1772@gmail.com>
2025-10-25 13:31:13 +0530

    Changed Settings

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT;
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://dict-pie.vercel.app',
     credentials: true,
 }));
 
